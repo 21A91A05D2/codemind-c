5afb0d9021b7c0f651016ab3e2e5ad38adbd54e5
21A91A05D2 <97424403+21A91A05D2@users.noreply.github.com>
2022-04-10 18:31:17 +0530

    #include<stdio.h> int reverse(int n) {     int rev=0,r;     while(n)     {         r=n%10;         rev=rev*10+r;         n=n/10;     }     return rev; }  int main()  {      int a,b;      scanf("%d%d",&a,&b);      int i;      for(i=a;i<=b;i++)      {          if(i==reverse(i))          printf("%d ",i);      }  }

---
+++ Program_to_print_all_palindromes_in_a_given_range_.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+int reverse(int n)
+{
+    int rev=0,r;
+    while(n)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    return rev;
+}
+ int main()
+ {
+     int a,b;
+     scanf("%d%d",&a,&b);
+     int i;
+     for(i=a;i<=b;i++)
+     {
+         if(i==reverse(i))
+         printf("%d ",i);
+     }
+ }
