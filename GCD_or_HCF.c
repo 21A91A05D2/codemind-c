e198b00bd0caa7b9788e910ed54ba4d0403ad44f
21A91A05D2 <97424403+21A91A05D2@users.noreply.github.com>
2022-04-23 21:14:49 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     while(a!=b)     {         if(a>b)         a=a-b;         else         b=b-a;     }     printf("%d",a); }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,19 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int a,b;
-	scanf("%d%d",&a,&b);
-	while(a!=b)
-	{
-	if(a>b)
-	{
-		a=a-b;
-	}
-	else
-	{
-		b=b-a;
-	}
+    int a,b;
+    scanf("%d%d",&a,&b);
+    while(a!=b)
+    {
+        if(a>b)
+        a=a-b;
+        else
+        b=b-a;
     }
-	printf("%d",a);
-	return 0;
+    printf("%d",a);
 }
