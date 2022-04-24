e3176734da25a2564e7f442ad95867543472ac6e
21A91A05D2 <97424403+21A91A05D2@users.noreply.github.com>
2022-04-24 16:36:36 +0530

    #include<stdio.h> int main() { 	int a,b,i=1; 	scanf("%d%d",&a,&b); 	while(i>0) 	{ 		if(i%a==0&&i%b==0) 		{ 		printf("%d",i); 		break; 	    } 	    i++; 	} 	return 0; }

--- LCM.c
+++ LCM.c
@@ -1,24 +1,16 @@
-#include <stdio.h>
-int main() {
- int a, b;
- //printf("Enter two numbers 
-");
- scanf("%d", &a);
- scanf("%d", &b);
-
- /*
- *LCM is always greater than or equal to the-
- *maximum of the given two numbers
- */
- int lcm = (a > b) ? a : b;
-
- //loop until divisible by both
- while(1){
- if(lcm%a==0 && lcm%b==0)
- break;
- lcm++;
- }
-
- //output the LCM
- printf("%d",lcm);
- return 0;}
+#include<stdio.h>
+int main()
+{
+	int a,b,i=1;
+	scanf("%d%d",&a,&b);
+	while(i>0)                                             
+	{
+		if(i%a==0&&i%b==0)
+		{
+		printf("%d",i);
+		break;
+	    }
+	    i++;
+	}
+	return 0;
+}
