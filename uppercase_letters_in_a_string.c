850054905330f9cb7bb6ab4361ae9df0f7954841
lalitha <97424403+21A91A05D2@users.noreply.github.com>
2022-05-06 07:14:15 +0530

    #include<stdio.h> #include<string.h> #include<ctype.h> int main() {     int i,c=0,l=0;     char str[100];     scanf("%[^ ]s",str);     while(str[i]!=NULL)     {         if(isupper(str[i]))         l++;         i++;     }
        printf("%d",l);
    }

---
+++ uppercase_letters_in_a_string.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main()
+{
+    int i,c=0,l=0;
+    char str[100];
+    scanf("%[^
+]s",str);
+    while(str[i]!=NULL)
+    {
+        if(isupper(str[i]))
+        l++;
+        i++;
+    }
+
+    printf("%d",l);
+}
